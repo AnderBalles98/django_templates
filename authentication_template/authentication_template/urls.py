3c7194828959b9f751d9b88fedea5f2b9886d18d
aballesterosr <aballesteros@vettica.co>
2022-04-10 15:08:38 -0500

    add rest-framework-jwt

--- authentication_template/authentication_template/urls.py
+++ authentication_template/authentication_template/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework_jwt.views import ObtainJSONWebToken
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', ObtainJSONWebToken.as_view()),
 ]
